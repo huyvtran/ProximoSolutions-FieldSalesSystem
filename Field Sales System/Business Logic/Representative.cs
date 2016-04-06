4b817e394179a4eb710459fcbf18366f740fb38b
Isuru Perera <tharanga.14@cse.mrt.ac.lk>
2016-04-07 00:37:29 +0530

    Completed 10 classes today. Wrapping up my work

---
+++ Field Sales System/Business Logic/Representative.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Field_Sales_System.Business_Logic
+{
+    class Representative
+    {
+    }
+}
