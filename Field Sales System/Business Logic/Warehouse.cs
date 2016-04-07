ffd2a4e39ffe54ec265ee413922744019733b7fd
fnote <sithijarox@yahoo.com>
2016-04-07 11:05:25 +0530

    Fnote first commit warehouse class

---
+++ Field Sales System/Business Logic/Warehouse.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Field_Sales_System.Business_Logic
+{
+    class Warehouse
+    {
+        private String warehouseName;
+        private String warehouseAddress;
+        private String memberName;
+
+        public void setwarehouseName(String warehouseName)
+        {
+            this.warehouseName = warehouseName;
+        }
+
+        public void setwarehouseAddress(String warehouseAddress)
+        {
+            this.warehouseAddress = warehouseAddress;
+        }
+
+        public void setmemberName(String memberName)
+        {
+            this.memberName = memberName;
+        }
+
+
+        public String getwarehouseName()
+        {
+            return this.warehouseName;
+        }
+
+        public String getwarehouseAddress()
+        {
+            return this.warehouseAddress;
+        }
+
+        public String getmemberName()
+        {
+            return this.memberName;
+        }
+
+
+    }
+}
