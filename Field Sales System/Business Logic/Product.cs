c969b7f61a3f6f9b402ed26175a1d50c29eca12c
Sajeewa <Sajeewa>
2016-04-07 13:26:15 +0530

    Suji's commit

---
+++ Field Sales System/Business Logic/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Field_Sales_System.Business_Logic
+{
+    class Product
+    {
+        private String productName;
+        private int productID;
+        private String productDescription;
+
+        public void setProductName(String productName)
+        {
+            this.productName = productName;
+        }
+
+        public String getProductName()
+        {
+            return productName;
+        }
+
+        public void setProductID(int productID)
+        {
+            this.productID = productID;
+        }
+        public int getProductID()
+        {
+            return productID;
+        }
+
+        public void setProductDescription(String productDescription)
+        {
+            this.productDescription = productDescription;
+        }
+        public String getProductDescription()
+        {
+            return productDescription;
+        }
+    }
+}
---
+++ Field Sales System/Business Logic/ProductDetails.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Field_Sales_System.Business_Logic
+{
+    class ProductDetails
+    {
+        private String category;
+        private int batchNO;
+        private String MFD;
+        private String EXP;
+
+        public void setCategory(String category)
+        {
+            this.category = category;
+        }
+        public String getCategory()
+        {
+            return category;
+        }
+
+        public void setBatchNO(int batchNO)
+        {
+            this.batchNO = batchNO;
+        }
+        public int getBatchNO()
+        {
+            return batchNO;
+        }
+
+        public void setMFD(String MFD)
+        {
+            this.MFD = MFD;
+        }
+        public String getMFD()
+        {
+            return MFD;
+        }
+
+        public void setEXP(String EXP)
+        {
+            this.EXP = EXP;
+        }
+        public String getEXP()
+        {
+            return EXP;
+        }
+    }
+}
