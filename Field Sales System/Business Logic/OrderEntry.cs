9f69422542eb8f569eb0c3ce270804aac43608db
Pasindu Senanayake <Pasindu Senanayake>
2016-04-07 13:41:06 +0530

    Test

---
+++ Field Sales System/Business Logic/OrderEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Field_Sales_System.Business_Logic
+{
+    class OrderEntry
+    {
+        private int qunatity;
+    }
+}
