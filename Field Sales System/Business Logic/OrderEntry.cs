02d89cb487a1e2857cf15173156da42056d9f792
Isuru Perera <tharanga.14@cse.mrt.ac.lk>
2016-04-07 14:19:03 +0530

    Fixed all

---
+++ Field Sales System/Business Logic/OrderEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Field_Sales_System.Business_Logic
+{
+    class OrderEntry
+    {
+        private int quantity;
+        //private Prdu
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+        public int getQuantity() {
+            return this.quantity;
+        }
+
+    }
+}
