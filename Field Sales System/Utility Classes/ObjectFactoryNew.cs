1b8dce660e37200e7082ba667c3f3d5bf37cbd85
pasindu <pasinduath@gmail.com>
2016-05-13 10:53:11 +0530

    Isuru we are good

--- Field Sales System/Utility Classes/ObjectFactoryNew.cs
+++ Field Sales System/Utility Classes/ObjectFactoryNew.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,7 @@ using System.Drawing;
 
 namespace Field_Sales_System.Utility_Classes
 {
-    class ObjectFactory
+    class ObjectFactoryNew
     {
         object send;
         public Object setObject(ArrayList inputset)
@@ -31,7 +31,8 @@ namespace Field_Sales_System.Utility_Classes
         }
 
 
-        private Object neworder(ArrayList inputset) {
+        private Object neworder(ArrayList inputset)
+        {
             Product orderedProduct = this.getObject(Convert.ToInt32(inputset[2]));
             Order neworder = new Order(Convert.ToInt32(inputset[0]), Convert.ToDateTime(inputset[1]));
             return neworder;
@@ -39,7 +40,8 @@ namespace Field_Sales_System.Utility_Classes
         }
 
         private StackTrace checker()
-        { StackTrace stackTrace = new StackTrace();
+        {
+            StackTrace stackTrace = new StackTrace();
             return stackTrace;
 
         }
@@ -61,27 +63,31 @@ namespace Field_Sales_System.Utility_Classes
         private Product getProduct()
         {
             return null;
-        }  
-        public User crerateUser(int empId, int empNIC, bool gender, string firstName, string lastName, int mobileNo, int landNo, string email, string addressLine_1, string addressLine_2, string addressLine_3, Image img,string userRole, List<Permission> permissions) {
+        }
+        public User crerateUser(int empId, int empNIC, bool gender, string firstName, string lastName, int mobileNo, int landNo, string email, string addressLine_1, string addressLine_2, string addressLine_3, Image img, string userRole, List<Permission> permissions)
+        {
             try
             {
 
                 //This class is not finished. Do not touch!
-                if (userRole.Equals("Representative")) {
+                if (userRole.Equals("Representative"))
+                {
                     User rep = new Representative(empId, empNIC, gender, firstName, lastName, mobileNo, landNo, email, addressLine_1, addressLine_2, addressLine_3, img);
                     ContactDetails c = new ContactDetails(mobileNo, landNo, email, addressLine_1, addressLine_2, addressLine_3);
                     UserRole ur = new UserRole();
                     ur.setRoleName(userRole);
-                    for (int i = 0; i < permissions.Count; i++) {
+                    for (int i = 0; i < permissions.Count; i++)
+                    {
                         ur.addPermission(permissions[i]);
                     }
-                    
-                    
+
+
                     return rep;
                 }
                 return null;
             }
-            catch (Exception e) {
+            catch (Exception e)
+            {
                 return null;
             }
         }
